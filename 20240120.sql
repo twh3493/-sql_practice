509e51ae5c5c10f4c6af7b30d5ee297c85888937
Vita Huang <vita94203@gmail.com>
2024-01-20 22:50:23 +0800

    SQL-子句用法

--- 20240120.sql
+++ 20240120.sql
@@ -5,7 +5,7 @@
 SELECT prod_name
 FROM products
 ORDER by prod_name;                     --使用"order by"會將數據照字母排列，語句內容也會跟前面相同
-                                        --需將"oder by"擺在最後一行，不然會出現錯誤
+                                        --需將"order by"擺在最後一行，不然會出現錯誤
                         
 SELECT prod_id, prod_price, prod_name 
 FROM Products 
@@ -14,7 +14,7 @@ ORDER BY prod_price, prod_name;         --這樣會先以價格排序，接著�
                                         
 SELECT prod_id, prod_price, prod_name 
 FROM Products 
-ORDER BY prod_price DESC;               --DESC為降序 (Z->A)      
+ORDER BY prod_price DESC;               --DESC為降序 (Z->A)  page 43    
 
 
 
@@ -36,4 +36,37 @@ SELECT vend_id, prod_name
 FROM Products 
 WHERE vend_id != 'DLL01';               --只顯示內容沒有DLL01的列
 
-WHERE prod_price IS NULL                --檢查產品內有沒有空格(不是找出價格為0的)
+WHERE prod_price IS NULL                --檢查產品內有沒有空格(不是找出價格為0的) page51
+
+
+
+-----Lesson 5-----
+
+--組合where字句，使用操作符AND或OR給出附加條件
+
+SELECT prod_id, prod_price, prod_name 
+FROM Products 
+WHERE vend_id = 'DLL01' AND prod_price <= 4;      --可以多加附加條件，中間記得使用AND即可
+                                                  --如AND和OR同時存在，會以AND的附加條件為優先
+                                                  --若要讓OR條件先，跟算式一樣需使用圓括號( )
+
+--使用IN來指定條件範圍，功能跟OR類似但較靈活
+
+SELECT prod_name, prod_price 
+FROM Products 
+WHERE vend_id IN ( 'DLL01', 'BRS01' )             
+ORDER BY prod_name;                        
+
+--使用NOT來否定後面的條件
+
+SELECT prod_name 
+FROM Products 
+WHERE NOT vend_id = 'DLL01'                         --除了DLL01之外的東西都否定，也就是只顯示DLL01相關
+ORDER BY prod_name;                                 --在簡單的子句中，可用<>取代NOT
+
+
+
+-----Lesson 6-----
+
+--Like 召喚包含關鍵字的所有值
+--% 表示任何自符出現任
